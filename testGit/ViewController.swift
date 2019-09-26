1a176348288fb67bcc44a76ec73fb9d67d685f42
Dan Ilinca <ilinca_danutts@yahoo.com>
2019-09-27 00:58:06 +0300

    Initial Commit

---
+++ testGit/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  testGit
+//
+//  Created by Dan Ilinca on 27/09/2019.
+//  Copyright © 2019 Dan Ilinca. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
